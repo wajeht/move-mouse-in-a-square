95a3dfe456adb22351a823de059e6663e72b3c4e
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-14 12:48:21 -0600

    refactor(index): simplify MouseMover initialization

--- src/index.js
+++ src/index.js
@@ -3,10 +3,4 @@
 import { mouse, left, right, up, down, keyboard } from '@nut-tree/nut-js';
 import { MouseMover } from './lib/mouse-mover.js';
 
-const m = new MouseMover(50, 1000, mouse, keyboard, {
-	left,
-	right,
-	up,
-	down,
-});
-await m.start();
+await new MouseMover(50, 1000, mouse, keyboard, { left, right, up, down }).start();
